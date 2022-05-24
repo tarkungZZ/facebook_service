05b32d9813448139d1e6cf8463d25d59816c0640
tarkungZZ <tarkungZ@gmail.com>
2022-05-24 14:57:07 +0700

    setup structure

--- helpers/gen_secret.js
+++
@@ -1,3 +0,0 @@
-const generate = require('crypto').randomBytes(64).toString('hex')
-
-console.log(generate)
--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const authorize = require('./middlewares/authorize')
-const { PORT } = require('./helpers/config')
-
-app.use(require('cors'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-//no authorize
-app.use('/login', require('./routes/user'))
-
-//authorize
-app.use('/facebook', authorize, require('./routes/facebook'))
-app.use('/config', authorize, require('./routes/config'))
-
-app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`) })
+//start bot console lisening event for work
