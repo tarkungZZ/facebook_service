6445d1c5efef600d0f3513d262cc3e083f670a6b
tarkungZZ <tarkungZ@gmail.com>
2022-05-30 16:48:12 +0700

    add post bot

--- controllers/components/like.js
+++ controllers/components/like.js
@@ -1,5 +1,4 @@
 const clipboardy = require('node-clipboardy')
-//const { scrollPageToBottom } = require('puppeteer-autoscroll-down')
 
 module.exports = async (puppeteer, data, config) => {
 
@@ -62,13 +61,6 @@ module.exports = async (puppeteer, data, config) => {
                 await page.click('#checkpointSubmitButton')
                 await delay(randomDelay)
                 await page.click('#checkpointSubmitButton')
-                await delay(randomDelay)
-                await page.click('#checkpointSubmitButton')
-                await delay(randomDelay)
-                await page.click('#checkpointSubmitButton')
-                await delay(randomDelay)
-                await page.keyboard.press('Escape')
-
                 break
 
             } catch (err) {
@@ -79,6 +71,32 @@ module.exports = async (puppeteer, data, config) => {
 
         }
 
+        //check verify birthdate selector
+
+        //#u_0_b_R6 > div._4-u2._5x_7._p0k._5x_9._a1-b._4-u8 > div._2ph_ > div > div > div:nth-child(1) > div > span
+        //#u_0_b_9y > div._4-u2._5x_7._p0k._5x_9._a1-b._4-u8 > div._2ph_ > div > div > div:nth-child(1) > div > span
+
+
+        //#u_c_1_st  //label input birthdate
+
+
+        await delay(5000)
+
+        for (let i = 0; i < 10; i++) {
+
+            try {
+
+                await page.waitForSelector(`[aria-label="สร้างโพสต์"]`, { timeout: 3000 })
+                break
+
+            } catch (err) {
+                if (err) {
+                    console.log(`Not in the feed yet , wait for 30s`)
+                    await delay(60000)
+                }
+            }
+
+        }
 
         console.log('Start farm like')
 
@@ -86,6 +104,13 @@ module.exports = async (puppeteer, data, config) => {
             farmlike(page)
             count++
         }, 30000)
+
+        // const statementButton = await page.$x("//button[contains(text(), 'คุณคิดอะไรอยู่')]")
+        // if (statementButton.length > 0) {
+
+        //     await statementButton[0].click()
+
+        // } else { throw new Error("Link not found") }
 
         // #mount_0_0_Sr > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(2) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div:nth-child(1) > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a
         // #mount_0_0_Sr > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(4) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a
@@ -95,30 +120,21 @@ module.exports = async (puppeteer, data, config) => {
 
         async function farmlike(page) {
 
-            //console.log('Scrolling')
-
-            // await scrollPageToBottom(page, {
-            //     size: 100,
-            //     delay: 100,
-            //     stepsLimit: 10
-            // })
-
-            for (let i = 0; i < 22; i++) {
-                await page.keyboard.press('ArrowDown', { delay: 50 })
-            }
-
             try {
 
-                console.log(count)
+                for (let i = 0; i < 22; i++) {
+                    await page.keyboard.press('ArrowDown', { delay: 50 })
+                }
+
+                //console.log(count)
                 const likeButton = await page.waitForSelector(`div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(${count}) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a`, { visible: true, timeout: 3000 })
-                console.log(`found like`)
+                //console.log(`found like`)
 
                 if ((Math.random() < 0.3) === true) {
                     await delay(randomDelay)
-                    console.log(`Like.`)
+                    //console.log(`Like.`)
                     await likeButton.click()
-
-                } else { console.log(`Pass.`) }
+                }
 
             } catch (err) {
 
@@ -127,19 +143,6 @@ module.exports = async (puppeteer, data, config) => {
             }
 
         }
-
-
-
-        //await page.waitForXPath("//button[contains(text(), 'ถูกใจ')]", { timeout: 30000 })
-        // const [button] = await page.$x("//div[@class='elements']/button[contains(., 'Button text')]")
-        // await button.click()
-
-        // const statementButton = await page.$x("//button[contains(text(), 'ถูกใจ')]")
-        // if (statementButton.length > 0) {
-
-        //     await statementButton[0].click()
-
-        // } else { throw new Error("Link not found") }
 
         function delay(time) {
             return new Promise((resolve) => {
