6687597b64a5fa308e0384888a782f2573cd0cee
tarkungZZ <tarkungZ@gmail.com>
2022-05-31 15:08:30 +0700

    add launch share

--- controllers/launch_button.js
+++ controllers/launch_button.js
@@ -5,7 +5,7 @@ const socket = io.connect(`http://${SOCKET_IP}:${SOCKET_PORT}`)
 
 module.exports = async (req, res) => {
 
-    const { id, type } = req.body
+    const { id, type, link } = req.body
 
     const getData = await pool(`SELECT id , email, fb_password, two_fa , execute_path FROM facebook_account WHERE id =? `, [id])
     const getConfig = await pool(`SELECT delay , delay_end FROM config WHERE id =?`, [1])
@@ -48,6 +48,22 @@ module.exports = async (req, res) => {
                 delay: getConfig[0].delay * 1000,
                 delay_end: getConfig[0].delay_end * 1000,
                 post
+            }
+
+        }
+
+        if (type === 'share') {
+
+            data = {
+                type,
+                id: getData[0].id,
+                email: getData[0].email,
+                fb_password: getData[0].fb_password,
+                two_fa: getData[0].two_fa,
+                execute_path: getData[0].execute_path,
+                delay: getConfig[0].delay * 1000,
+                delay_end: getConfig[0].delay_end * 1000,
+                link
             }
 
         }
