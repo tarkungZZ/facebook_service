a1e80dcca56849cb162c3a1a30a426a6a7a8849f
tarkungZZ <61244857+tarkungZZ@users.noreply.github.com>
2022-06-07 17:19:52 +0700

    restructure main program

--- controllers/components/like.js
+++ controllers/components/like.js
@@ -1,153 +1,21 @@
-const clipboardy = require('node-clipboardy')
+const delay = require('../../helpers/delay')
 
-module.exports = async (puppeteer, data, config) => {
+module.exports = async (page, randomDelay, count) => {
 
     try {
 
-        let count = 1
-
-        const randomDelay = Math.floor(Math.random() * (data.delay_end - data.delay) + data.delay)
-
-        const browser = await puppeteer.launch({
-            headless: false,
-            executablePath: data.execute_path,
-            slowMo: 10,
-            args: [
-                //`--window-size=1024,768`,
-                '--no-sandbox',
-                "--disable-notifications",
-                '--disable-translate'
-            ]
-        })
-
-        const page = (await browser.pages())[0]
-        // await page.setViewport({
-        //     width: 1020,
-        //     height: 764,
-        //     deviceScaleFactor: 1
-        // })
-
-        await page.goto(config.mfb_url, { waitUntil: 'networkidle2' })
-        await page.goto(config.fb_url, { waitUntil: 'networkidle2' })
-
-        await page.waitForSelector('#email')
-        await page.type('#email', data.email)
-        await delay(randomDelay)
-        await page.type('#pass', data.fb_password)
-        await delay(randomDelay)
-        const loginButton = await page.waitForSelector('button[type="submit"][name="login"]')
-        await loginButton.click()
-
-        const page2 = await browser.newPage()
-        await page2.goto(config.two_fa_url)
-        await page2.type('#listToken', data.two_fa)
-        await delay(randomDelay)
-        await page2.click('#submit')
-        await delay(randomDelay)
-        await page2.click('#copy_btn', { clickCount: 2 })
-
-        const clipboardtext = await clipboardy.readSync()
-        const two_fa_code = clipboardtext.slice(-6)
-        await page2.close()
-        //console.log(two_fa_text)
-        await delay(randomDelay)
-        await page.type('#approvals_code', two_fa_code)
-
-        for (let i = 0; i < 5; i++) {
-
-            try {
-
-                await delay(randomDelay)
-                await page.click('#checkpointSubmitButton')
-                await delay(randomDelay)
-                await page.click('#checkpointSubmitButton')
-                break
-
-            } catch (err) {
-
-                if (err) { break }
-
-            }
-
+        for (let i = 0; i < 22; i++) {
+            await page.keyboard.press('ArrowDown', { delay: 50 })
         }
 
-        //check verify birthdate selector
+        //console.log(count)
+        const likeButton = await page.waitForSelector(`div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(${count}) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a`, { visible: true, timeout: 3000 })
+        //console.log(`found like`)
 
-        //#u_0_b_R6 > div._4-u2._5x_7._p0k._5x_9._a1-b._4-u8 > div._2ph_ > div > div > div:nth-child(1) > div > span
-        //#u_0_b_9y > div._4-u2._5x_7._p0k._5x_9._a1-b._4-u8 > div._2ph_ > div > div > div:nth-child(1) > div > span
-
-
-        //#u_c_1_st  //label input birthdate
-
-
-        await delay(5000)
-
-        for (let i = 0; i < 10; i++) {
-
-            try {
-
-                await page.waitForSelector(`[aria-label="สร้างโพสต์"]`, { timeout: 3000 })
-                break
-
-            } catch (err) {
-                if (err) {
-                    console.log(`Not in the feed yet , wait for 30s`)
-                    await delay(60000)
-                }
-            }
-
-        }
-
-        console.log('Start farm like')
-
-        setInterval(async () => {
-            farmlike(page)
-            count++
-        }, 30000)
-
-        // const statementButton = await page.$x("//button[contains(text(), 'คุณคิดอะไรอยู่')]")
-        // if (statementButton.length > 0) {
-
-        //     await statementButton[0].click()
-
-        // } else { throw new Error("Link not found") }
-
-        // #mount_0_0_Sr > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(2) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div:nth-child(1) > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a
-        // #mount_0_0_Sr > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(4) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a
-        // #mount_0_0_Sr > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(6) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a
-        // #mount_0_0_Sr > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(7) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a
-        // #mount_0_0_Sr > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(8) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a
-
-        async function farmlike(page) {
-
-            try {
-
-                for (let i = 0; i < 22; i++) {
-                    await page.keyboard.press('ArrowDown', { delay: 50 })
-                }
-
-                //console.log(count)
-                const likeButton = await page.waitForSelector(`div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div.j83agx80.btwxx1t3.taijpn5t > div > div.pedkr2u6.tn0ko95a.pnx7fd3z > div > div:nth-child(${count}) > div > div > div > div > div > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div > div > div > div.tvfksri0.ozuftl9m > div > div:nth-child(1) > div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl.gokke00a`, { visible: true, timeout: 3000 })
-                //console.log(`found like`)
-
-                if ((Math.random() < 0.3) === true) {
-                    await delay(randomDelay)
-                    //console.log(`Like.`)
-                    await likeButton.click()
-                }
-
-            } catch (err) {
-
-                //console.log(err)
-
-            }
-
-        }
-
-        function delay(time) {
-            return new Promise((resolve) => {
-                setTimeout(resolve, time)
-            })
+        if ((Math.random() < 0.3) === true) {
+            await delay(randomDelay)
+            console.log(`Like.`)
+            await likeButton.click()
         }
 
     } catch (err) {
@@ -157,5 +25,3 @@ module.exports = async (puppeteer, data, config) => {
     }
 
 }
-
-
--- controllers/components/random.js
+++ controllers/components/random.js
@@ -1,18 +1,6 @@
 const clipboardy = require('node-clipboardy')
-const puppeteer = require('puppeteer-extra')
-const stealth = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(stealth())
-const config = require('../helpers/config')
-const delay = require('../helpers/delay')
-const farmLike = require('./components/like')
-const farmPost = require('./components/post')
-const farmShare = require('./components/share')
-const farmStory = require('./components/story')
-const farmRandom = require('./components/random')
 
-module.exports = async (data) => {
-
-    //console.log(data)
+module.exports = async (puppeteer, data, config) => {
 
     try {
 
@@ -101,34 +89,76 @@ module.exports = async (data) => {
 
         }
 
-        if (data.type === 'like') {
-            console.log(`Start farming like.`)
-            setInterval(() => {
-                count++
-                farmLike(page, randomDelay, count)
-            }, 30000)
-        }
+        console.log('Start random farm.')
 
-        if (data.type === 'story') {
-            console.log(`Start farming story.`)
-            const farm = setInterval(async () => {
-                console.log(count)
-                if (count >= 6) {
-                    clearInterval(farm)
-                    console.log(`Finish farm story loop.`)
-                    await delay(randomDelay)
-                    await browser.close()
+        randomFarm(page)
+
+        async function randomFarm(page) {
+
+            for (let i = 0; i < 5; i++) {
+
+                try {
+
+                    if (i === 4) {
+
+                        await browser.close()
+
+                        break
+
+                    }
+
+                    await delay(Math.floor(Math.random() * (15000 - 5000) + 5000))
+
+                    const emote = ['ถูกใจ', 'รักเลย', 'ห่วงใย', 'ว้าว']
+
+                    const random = Math.floor(Math.random() * emote.length)
+
+                    const randomEmote = `[aria-label="${emote[random]}"]`
+
+                    //console.log(`click story number ${i + 1}`)
+
+                    await page.click(`div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.pfnyh3mw.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.dp1hu0rb.p01isnhg > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.buofh1pr.g5gj957u.hpfvmrgz.taijpn5t.gs1a9yip.owycx6da.btwxx1t3.pmt1y7k9.f7vcsfb0.fjf4s8hc.b6rwyo50.oyrvap6t > div > div > div > div:nth-child(3) > div > div > div > div.oh7imozk.abvwweq7.ejjq64ki.d2edcug0 > div > div > div > div.rq0escxv.l9j0dhe7.du4w35lb.obtkqiv7.sv5sfqaa.sj5x9vvc.cxgpxx05 > div > div > div:nth-child(${i + 3})`)
+                    await delay(Math.floor(Math.random() * (8000 - 1000) + 3000))
+
+                    try {
+
+                        //console.log(`check if element appear`)
+
+                        await page.waitForSelector(`[aria-label="ถูกใจ"]`, { timeout: 3000 })
+                        //console.log(`found emote`)
+                        await page.click(randomEmote)
+                        //console.log(`sent emote`)
+                        await delay(randomDelay)
+                        await page.keyboard.press('Escape')
+
+                    } catch (err) {
+
+                        if (err) {
+                            //console.log('err in', err)
+                            await page.keyboard.press('Escape')
+                            continue
+                        }
+
+                    }
+
+                } catch (err) {
+
+                    if (err) {
+                        //console.log('err out', err)
+                        break
+                    }
+
                 }
-                farmStory(page, randomDelay, count)
-                count++
-            }, 30000)
+
+            }
+
         }
 
-        if (data.type === 'post') { farmPost(puppeteer, data, config) }
-
-        if (data.type === 'share') { farmShare(puppeteer, data, config) }
-
-        if (data.type === 'all') { farmRandom(puppeteer, data, config) }
+        function delay(time) {
+            return new Promise((resolve) => {
+                setTimeout(resolve, time)
+            })
+        }
 
     } catch (err) {
 
@@ -137,3 +167,5 @@ module.exports = async (data) => {
     }
 
 }
+
+
