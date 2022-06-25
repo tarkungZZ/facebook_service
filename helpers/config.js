b4a954f451d497e61eda65b0bea7533432d7a042
tarkungZZ <61244857+tarkungZZ@users.noreply.github.com>
2022-06-25 13:14:55 +0700

    remove encode

--- helpers/config.js
+++ helpers/config.js
@@ -10,7 +10,6 @@ module.exports = {
     PORT: process.env.PORT,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRE: process.env.JWT_EXPIRE,
-    MD5_SECRET: process.env.MD5_SECRET,
     SOCKET_IP: process.env.SOCKET_IP,
     SOCKET_PORT: process.env.SOCKET_PORT
 
