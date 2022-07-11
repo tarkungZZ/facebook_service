b7c6e2d1f7f3764997b904b0265c1f9ffae53948
tarkungZZ <61244857+tarkungZZ@users.noreply.github.com>
2022-07-11 11:05:02 +0700

    headless

--- controllers/main.js
+++ controllers/main.js
@@ -63,7 +63,7 @@ module.exports = async (data) => {
         try {
 
             const browser = await puppeteer.launch({
-                headless: false,
+                headless: true,
                 defaultViewport: null,
                 //executablePath: obj.execute_path,
                 slowMo: 10,
