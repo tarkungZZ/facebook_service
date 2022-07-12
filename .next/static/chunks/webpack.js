666b845ccc30c7481a625c4dc54c8195d159fe84
OSEJAMES <james.11350@gmail.com>
2022-07-12 14:17:14 +0700

    switch Lang

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5313bb0f254f2239"; }
+/******/ 		__webpack_require__.h = function() { return "013af7a05b88b9fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
