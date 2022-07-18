df6813bbcbe86f1b1ec7b90ac7c2365c0618e472
tarkungZZ <61244857+tarkungZZ@users.noreply.github.com>
2022-07-18 17:22:14 +0700

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const authorize = require('./middlewares/authorize')
 const { PORT } = require('./helpers/config')
 
-app.use(require('cors')())
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
