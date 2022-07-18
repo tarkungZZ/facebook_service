dde3c8898c0c54d59385a1898da17e3348c713ba
tarkungZZ <61244857+tarkungZZ@users.noreply.github.com>
2022-07-18 18:18:54 +0700

    rollback cors

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const bodyParser = require('body-parser')
 const authorize = require('./middlewares/authorize')
 const { PORT } = require('./helpers/config')
 
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:3001'
-}))
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
