b0b545679fad51afaecc8ca48369cd136b6eb497
Supagorn Srichan <armsrichan2@gmail.com>
2022-07-18 19:16:02 +0700

    test socket connect

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,39 +1,30 @@
 import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
+import io from "socket.io-client";
 
-const ENDPOINT = "http://159.223.53.175:5002"
+const ENDPOINT = "http://159.223.53.175:5002";
 
 function App() {
   const [lastPong, setLastPong] = useState(null);
 
   useEffect(() => {
-
-    const socket = socketIOClient(ENDPOINT);
+    const socket = io(ENDPOINT);
     socket.on("connect", () => {
       console.log("connected");
       // setIsConnected(true);
     });
 
     socket.on("disconnect", () => {
-      console.log("disconnect")
+      console.log("disconnect");
       // setIsConnected(false);
     });
 
-    socket.on("bot-status", () => {
-      setLastPong(new Date().toISOString());
+    socket.on("bot-status", (data) => {
+      console.log("data ===> ", data);
+      // setLastPong(new Date().toISOString());
     });
-
-    return () => {
-      socket.off("connect");
-      socket.off("disconnect");
-      socket.off("bot-status");
-    };
   }, []);
 
-  return (
-    <div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
