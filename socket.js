08d887a322410530801730fe1d70e21685a85618
tarkungZZ <61244857+tarkungZZ@users.noreply.github.com>
2022-07-19 15:13:13 +0700

    test new socket

--- socket.js
+++ socket.js
@@ -1,15 +1,37 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
-    cors: {
-        origin: "http://159.223.53.175:3001",
-        methods: ["GET", "POST"],
-        credentials: false
+    // cors: {
+    //     origin: "http://159.223.53.175:3001",
+    //     methods: ["GET", "POST"],
+    //     credentials: false
+    // }
+
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers)
+        res.end()
     }
+
 })
+
+handlePreflightRequest: (req, res) => {
+    const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+        "Access-Control-Allow-Credentials": true
+    };
+    res.writeHead(200, headers);
+    res.end();
+}
+
+
 const { SOCKET_PORT } = require('./helpers/config')
 const updateStatus = require('./helpers/update_status')
-
 
 io.on('connection', (socket) => {
 
@@ -23,7 +45,7 @@ io.on('connection', (socket) => {
     socket.on('status', (data) => {
         console.log(`Update facebook working status.`)
         updateStatus(data)
-        socket.emit('bot-status', data)
+        socket.emit('bot-status', { data })
     })
 
     socket.on('disconnect', () => {
@@ -31,7 +53,6 @@ io.on('connection', (socket) => {
     })
 
 })
-
 
 http.listen(SOCKET_PORT, () => { console.log(`Socket server listening on port ${SOCKET_PORT}`) })
 
