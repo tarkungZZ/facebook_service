d29b11ba42f9896972fb58294b8b9c2b088536f1
tarkungZZ <61244857+tarkungZZ@users.noreply.github.com>
2022-07-19 15:18:12 +0700

    rollback

--- socket.js
+++ socket.js
@@ -1,34 +1,12 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
-    // cors: {
-    //     origin: "http://159.223.53.175:3001",
-    //     methods: ["GET", "POST"],
-    //     credentials: false
-    // }
-
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers)
-        res.end()
+    cors: {
+        origin: "http://159.223.53.175:3001",
+        methods: ["GET", "POST"],
+        credentials: false
     }
-
 })
-
-handlePreflightRequest: (req, res) => {
-    const headers = {
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-        "Access-Control-Allow-Credentials": true
-    };
-    res.writeHead(200, headers);
-    res.end();
-}
-
 
 const { SOCKET_PORT } = require('./helpers/config')
 const updateStatus = require('./helpers/update_status')
