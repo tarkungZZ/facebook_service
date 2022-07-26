99ab535368522cbda6c98d353e9f30fc5fc3b912
tarkungZZ <61244857+tarkungZZ@users.noreply.github.com>
2022-07-26 18:10:07 +0700

    change debug bot name

--- socket_debug.js
+++ socket_debug.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client')
 const { SERVER_SOCKET_IP, SERVER_SOCKET_PORT } = require('./helpers/config')
 const socket = io.connect(`http://${SERVER_SOCKET_IP}:${SERVER_SOCKET_PORT}`)
-const main = require('./controllers/main_no_headless')
+const main = require('./controllers/main_debug')
 
 socket.on('client-connection', (msg) => { console.log(msg) })
 
